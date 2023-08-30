66505d72a99cdb2abbc5944d7ed734bb8c5e2a92
enas48 <enasm4829@gmail.com>
2023-08-30 10:51:43 +0300

    add video playlist

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const dotenv = require('dotenv').config();
 const connectDB = require('./config/db');
-
+const cors = require('cors');
 const port = process.env.PORT || 8000;
 
 connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended:false}));
+app.use(cors({origin:"*"}))
 
 const bookmarkRouter = require('./routes/BookmarkRoutes');
 const favouriteRouter=require('./routes/favouriteRoutes');
